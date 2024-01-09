a2e4fa00ba1eebd099094b06a643f4ad23766cc9
cmmiclat <142866810+cmmiclat@users.noreply.github.com>
2024-01-09 14:41:03 +0800

    Add files via upload

---
+++ components/Welcome.jsx
@@ -0,0 +1,10 @@
+export default class Welcome extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Welcome</h1>
+                <p>to my website</p>
+            </div>
+        )
+    }
+}
